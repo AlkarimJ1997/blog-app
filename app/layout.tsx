8419b8227849faeda000e8d752bbe5f84cd9c661
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-06-23 14:57:04 -0700

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import { Navbar } from '@/components';
 
-const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Blogify',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className={nunito.className}>
 				<Navbar />
 				{children}
 			</body>
